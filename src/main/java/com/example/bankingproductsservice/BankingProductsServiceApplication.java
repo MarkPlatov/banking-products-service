cf0eede4454326834623a48e8e02f574ba109d43
MarkPlatov <mister.agamemnon@mail.ru>
2022-03-26 15:16:36 +0300

    Initial commit

---
+++ src/main/java/com/example/bankingproductsservice/BankingProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankingproductsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingProductsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingProductsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankingproductsservice/BankingProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankingproductsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingProductsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
